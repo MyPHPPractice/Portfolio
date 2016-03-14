4ef91e8686188660206774d10888216492549975
will <willsmith12289@gmail.com>
2016-03-14 06:14:19 -0400

    made php
    organized code with php and scss files

---
+++ footer.php
@@ -0,0 +1,24 @@
+<footer>
+
+		<address>
+			Two Barn Farm <br>
+			717 west sidney road<br>
+			Pittstown NJ, 08867<br><br>
+			&copy;willysmith<br>
+		</address>
+
+
+	<div class="nav">
+		<ul>
+			<li><a href="">contact</a></li>
+			<li><a href="">employment</a></li>
+			<li><a href="">sitemap</a></li>
+			<li><a href="">about</a></li>
+		</ul>
+	</div>
+
+</footer>
+
+</body>
+
+</html>
---
+++ nav.php
@@ -0,0 +1,9 @@
+<nav class="outer"> 
+	<ul class="sub">
+		<li><a href="#">Meet Will</a></li>
+		<li><a href="#">Projects</a></li>
+		<li><a href="#">Experience</a></li>
+		<li><a href="contact.php">Contact</a></li>
+
+	</ul>
+</nav>
