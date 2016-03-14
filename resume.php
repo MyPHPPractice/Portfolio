2d3e132fc025569a60e7c93a8190db14acd5f8e2
will <willsmith12289@gmail.com>
2016-03-14 09:52:36 -0400

    responsive nav

---
+++ resume.php
@@ -0,0 +1,85 @@
+<?php include 'head.php'; 
+include 'nav.php';
+include 'header.php';
+?>
+<section>
+	<h1>William Smith</h1>
+	<h3>Web Developer</h3>
+	<article>
+		<ul>
+			<li>Detail oriented junior developer with an outstanding will to learn</li>
+			<li>Excellent communication skills </li>
+			<li>Experience organizing large projects using logical problem solving </li>
+			<li>Proficient in HTML and CSS and growing knowledge of Javascript and its libraries, PHP, MySQL, Git, Sass, and Wordpress</li>
+			<li>Looking for an opportunity to expand my knowledge, and further my web development career</li>
+		</ul>
+		<h2>Education</h2>
+			<h1>Alfred University</h1>
+				<p>August 2008 - May 2010 48 credit hours</p>
+			<h1>Raritan Valley Community College</h1>
+			<h4>Associates Degree in Web Development</h4>
+				<p>June 2015 - Degree expected May 2016</p>
+			<h1>Courses:</h1>
+			<ul>
+				<li><b>Web Development 1 + 2</b>
+					<ul>
+						<li>HTML5, CSS3, JQuery</li>
+						<li>Good SEO practices</li>
+						<li>Cross browser compatibility</li>
+					</ul>
+				</li>
+				<li><b>Advanced Web and Mobile Development</b>
+					<ul>
+						<li>Responsive design</li>
+						<li>GitHub</li>
+					</ul>
+				</li>
+				<li><b>Systems Analysis and Design</b>
+					<ul>
+						<li>Systems Development Life Cycle models
+							<ul>
+								<li>RAD, Agile, Scrum, </li>
+							</ul>
+						</li>
+					</ul>
+
+				</li>
+				<li><b>Interactive Multimedia</b>
+					<ul>
+						<li>Learned the Adobe Suite and design process</li>
+					</ul>
+				</li>
+				<li><b>Privacy, Ethics, and Computer Forensics</b></li>
+				<li><b>Computer Concepts and Programming</b>
+					<ul>
+						<li>Basic OOP with Java</li>
+					</ul>
+				</li>
+				<li><b>JavaScript</b>
+					<ul>
+						<li>Design Patterns</li>
+						<li>Basic AJAX Calls</li>
+					</ul>
+				</li>
+				<li><b>Internet and Web Architecture</b>
+					<ul>
+						<li>Web/server management</li>
+					</ul>
+				</li>
+				<li><b>PHP</b>
+					<ul>
+						<li>Form handling</li>
+						<li>Basic MySQL database programming</li>
+					</ul>
+				</li>
+				<li><b>Content Management Systems</b>
+					<ul>
+						<li>Wordpress, some Drupal</li>
+						<li>CMS database management</li>
+						<li>Google Analytics</li>
+					</ul>
+				</li>
+			</ul>
+	</article>
+</section>
+<?php include 'footer.php'; ?>
